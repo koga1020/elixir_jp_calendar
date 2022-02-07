b248d82adff9938b03d651f0443449e25c58d028
koga1020 <koga.shozo1020@gmail.com>
2022-02-07 22:54:48 +0900

    add config

--- config/test.exs
+++ config/test.exs
@@ -15,3 +15,5 @@ config :logger, level: :warn
 
 # Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
+
+config :ex_connpass, api_base_url: "http://localhost:8081/
